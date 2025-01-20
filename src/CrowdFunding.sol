ec1aa82fd0a4b8d9dbc8f9349d473dd89b56be5d
rahulj1011 <rahuljr2022@gmail.com>
2025-01-20 22:38:23 +0530

    Added tiers and refund function

--- src/CrowdFunding.sol
+++ src/CrowdFunding.sol
@@ -10,22 +10,33 @@ contract CrowdFunding{
     enum CampaignState {
         Active,Successfull,Failed
     }
-
+    CampaignState public state;
     struct Tier{
 
         string name;
         uint256 amount;
         uint256 backers;
-        
+    }
+
+    struct Backer{
+      
+        uint256 totalContribution;
+        mapping(uint256 => bool)fundedTiers;
     }
 
     Tier[] public tier ;
+    mapping(address => Backer) public backers;
 
     modifier onlyOwner()
     {
         require(msg.sender == owner, "Only the owner can perform this action");
         _;
 
+    }
+    modifier campaignOpen()
+    {
+        require(state == CampaignState.Active, "The campaign is not active");
+        _;
     }
      
     constructor(string memory _name, string memory _description,uint256 _goal,uint256 _durationIndays){
@@ -34,14 +45,32 @@ contract CrowdFunding{
         goal = _goal;
         deadline = block.timestamp + (_durationIndays*1 days);
         owner = msg.sender;
+        state=CampaignState.Active;
     }
 
-    function fund(uint256 _tierIndex) public payable{
-       
+    function checkAndUpdate() internal {
+        if(state==CampaignState.Active)
+        {
+            if(block.timestamp >= deadline){
+                state = address(this).balance >= goal ? CampaignState.Successfull : CampaignState.Failed;
+
+            }
+            else{
+                state = address(this).balance >= goal ? CampaignState.Successfull : CampaignState.Active;
+
+            }
+        }
+    }
+
+    function fund(uint256 _tierIndex) public payable campaignOpen{
+        
         require(block.timestamp < deadline,"Fundraising period has ended");
         require(_tierIndex < tier.length," Invalid tier");
         require(msg.value == tier[_tierIndex].amount,"Incorrect amount");
         tier[_tierIndex].backers++;
+        backers[msg.sender].totalContribution+= msg.value;
+        backers[msg.sender].fundedTiers[_tierIndex] = true;
+        checkAndUpdate();
        
     }
 
@@ -60,7 +89,8 @@ contract CrowdFunding{
     }
 
     function withdraw() public onlyOwner{
-       
+        checkAndUpdate();
+        require(state == CampaignState.Successfull,"Campaign state is not sucessfull");
         require(address(this).balance >= goal,"Goal had not been reach");
         
         uint256 balance = address(this).balance;
@@ -73,5 +103,17 @@ contract CrowdFunding{
 
         return address(this).balance;   
     }
+    function refund()public {
+        checkAndUpdate();
+        require(state == CampaignState.Failed,"Refunds not allowed");
+        uint256 amount = backers[msg.sender].totalContribution;
+        require(amount > 0,"No contribution to refund");
+        backers[msg.sender].totalContribution=0;
+        payable(msg.sender).transfer(amount);
 
+    }
+
+    function hasFunded(address _backer,uint256 _tierIndex)public view returns(bool){
+        return backers[_backer].fundedTiers[_tierIndex];
+    }
 }
